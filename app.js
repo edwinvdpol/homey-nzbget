4136302b8cc3fa686df894226d95123a5c29b06a
Edwin van de Pol <developer@edwinvandepol.nl>
2021-05-24 21:17:28 +0200

    Added condition flowcard

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ class NZBApp extends Homey.App {
     this.log('NZBApp is running...');
 
     await this.registerActions();
+    await this.registerConditions();
   }
 
   // Register flowcard actions
@@ -41,6 +42,14 @@ class NZBApp extends Homey.App {
     // ... then shutdown ...
     this.homey.flow.getActionCard('shutdown').registerRunListener(async (args) => {
       return args.device.shutdown();
+    });
+  }
+
+  // Register flowcard conditions
+  async registerConditions() {
+    // ... and download queue is active...
+    this.homey.flow.getConditionCard('download_enabled').registerRunListener(async (args) => {
+      return args.device.getCapabilityValue('download_enabled') === true;
     });
   }
 
