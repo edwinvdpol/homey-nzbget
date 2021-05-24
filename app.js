35db8a8407b808772ce25b9ac138f544c4f7ffc0
Edwin van de Pol <developer@edwinvandepol.nl>
2021-05-24 16:03:13 +0200

    Homey SDK 3

--- app.js
+++ app.js
@@ -14,7 +14,51 @@ class NZBApp extends Homey.App {
   */
 
   async onInit() {
-    this.log('Running');
+    this.log('NZBApp is running...');
+
+    // Register flowcard actions
+    await this.registerActions();
+  }
+
+  /*
+  |---------------------------------------------------------------------------
+  | Register flowcard actions
+  |---------------------------------------------------------------------------
+  |
+  | Register flowcard actions which can be used in Homey 'Then' section.
+  |
+  */
+
+  async registerActions() {
+    // ... then pause download queue ...
+    this.homey.flow.getActionCard('pausedownload').registerRunListener(async (args) => {
+      return args.device.pausedownload();
+    });
+
+    // ... then set download speed limit ...
+    this.homey.flow.getActionCard('rate').registerRunListener(async (args) => {
+      return args.device.rate(args);
+    }).getArgument('download_rate');
+
+    // ... then reload server ...
+    this.homey.flow.getActionCard('reload').registerRunListener(async (args) => {
+      return args.device.reload();
+    });
+
+    // ... then resume download queue ...
+    this.homey.flow.getActionCard('resumedownload').registerRunListener(async (args) => {
+      return args.device.resumedownload();
+    });
+
+    // ... then scan incoming directory for nzb-files ...
+    this.homey.flow.getActionCard('scan').registerRunListener(async (args) => {
+      return args.device.scan();
+    });
+
+    // ... then shutdown ...
+    this.homey.flow.getActionCard('shutdown').registerRunListener(async (args) => {
+      return args.device.shutdown();
+    });
   }
 
 }
